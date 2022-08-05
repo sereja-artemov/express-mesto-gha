6dcf2a401768703b383c26b009fa57928523e33e
sereja-artemov <sereja.artemov.art@yandex.ru>
2022-08-05 17:21:06 +0300

    Добавил первоначальные настройки и подключение к серверу Mongo

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
+app.listen(3000);
