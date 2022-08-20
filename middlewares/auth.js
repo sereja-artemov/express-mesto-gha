93d988560ea28e2c66d8e9815b9da5a8d2e30609
sereja-art <sereja.artemov.art@yandex.ru>
2022-08-20 13:59:55 +0300

    refactor: Для проверки токена пользователя теперь беру токен не из заголовков, а из куки

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,13 +3,14 @@ const jwt = require('jsonwebtoken');
 const { JWT_SECRET } = process.env;
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(401).send({ message: 'Необходима авторизация' });
-  }
+  // const { authorization } = req.headers;
+  //
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   return res.status(401).send({ message: 'Необходима авторизация' });
+  // }
   // извлечём токен
-  const token = authorization.replace('Bearer ', '');
+  // const token = authorization.replace('Bearer ', '');
+  const token = req.cookies.jwt;
   let payload;
 
   try {
